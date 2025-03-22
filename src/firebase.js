b3d3f32c57957da6f3f755b11b2d29a488c2aa8d
Ben Kelman <b.a.kelman@gmail.com>
2025-03-22 16:35:24 -0700

    security fixes

--- src/firebase.js
+++ src/firebase.js
@@ -9,14 +9,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBBT0o-O0gC9A3RlRecc4L5YfnHATGCgxI",
-  authDomain: "ai-nutrition-logger.firebaseapp.com",
-  projectId: "ai-nutrition-logger",
-  storageBucket: "ai-nutrition-logger.firebasestorage.app",
-  messagingSenderId: "347512920796",
-  appId: "1:347512920796:web:327d5fb86143a842587219",
-  measurementId: "G-K8002RZZC5"
-};
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
